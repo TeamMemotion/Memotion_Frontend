62ad6566508f076ad1b928393db76e8a7816cff1
eojin0814 <leeeojin6@gmail.com>
2023-10-24 00:27:40 +0900

    [feat] route-detail

--- app/src/main/java/com/example/memotion/route/RouteActivity.java
+++ app/src/main/java/com/example/memotion/route/RouteActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.memotion.R;
@@ -17,6 +18,8 @@ public class RouteActivity extends AppCompatActivity {
     RouteDetailItem routeDetailItem;
     private RecyclerView recyclerView;
     private ActivityRouteBinding activityRouteBinding;
+
+    private RouteRecyclerAdapter routeAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +28,12 @@ public class RouteActivity extends AppCompatActivity {
 
         Intent intent = getIntent(); /*데이터 수신*/
         int routeId = intent.getExtras().getInt("routeId");
+
+        recyclerView=activityRouteBinding.horizontalRecyclerView;
+        routeAdapter = new RouteRecyclerAdapter(this);
+        recyclerView.setAdapter(routeAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
 
     }
 

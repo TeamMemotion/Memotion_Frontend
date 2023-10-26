79edcbcff28767ba1dbc17053b459268c65cd735
aeeazip <codnjs0809@naver.com>
2023-10-27 00:31:45 +0900

    fix: 깃 캐시 삭제 #wonny

--- app/src/main/java/com/example/memotion/account/login/LoginService.java
+++ app/src/main/java/com/example/memotion/account/login/LoginService.java
@@ -66,26 +66,41 @@ public class LoginService {
     public void kakaoLogin(LoginRequest.KakaoLogin loginRequest) {
          LoginRetrofitInterface loginService = getRetrofit().create(LoginRetrofitInterface.class);
          loginService.kakaoLogin(loginRequest).enqueue(new Callback<LoginResponse>() {
+
              // 성공한 경우
              @Override
              public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
-                 Log.d("KAKAO-LOGIN SUCCESS", response.toString());
-                 LoginResponse resp = response.body();
+                 Log.d("KAKAO-LOGIN-SUCCESS", response.toString());
+                 //FindPwdPostResponse resp = response.body();
 
-                 switch(resp.getCode()) {
-                     case 1000:
-                         loginResult.loginSuccess(resp.getCode(), resp.getResult());
-                         break;
-                     default:
-                         loginResult.loginFailure(resp.getCode(), resp.getMessage());
-                         break;
+                 if(response.isSuccessful()) {
+                     if(response.body().getCode() == 1000) {
+                         loginResult.loginSuccess(response.body().getCode(), response.body().getResult());
+                     }
+                 } else {        //400이상 에러시 response.body가 null로 처리됨. 따라서 errorBody로 받아야함.
+                     try {
+                         String errorBody = response.errorBody().string();
+                         NetworkModule.ErrorResponse errorResponse = errorParsing(errorBody);
+
+                         Log.d("ErrorBody : ", errorBody);
+                         Log.d("errorCode: ", String.valueOf(errorResponse.getCode()));
+                         Log.d("errorMessage: ", errorResponse.getMessage());
+
+                         switch (errorResponse.getCode()) {
+                             case 500:
+                             case 2001:
+                                 loginResult.loginFailure(errorResponse.getCode(), errorResponse.getMessage());
+                                 break;
+                         }
+                     } catch (IOException e) {
+                         e.printStackTrace();
+                     }
                  }
              }
 
-             // 실패한 경우
              @Override
              public void onFailure(Call<LoginResponse> call, Throwable t) {
-                Log.d("KAKAO-LOGIN FAILURE", t.getMessage().toString());
+                 Log.d("KAKAO-LOGIN-FAILURE", t.getMessage());
              }
          });
     }
